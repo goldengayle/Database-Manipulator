f8c098b2c371671cf6d666229e13a8cd6c6e448d
goldie <codegayle@gmail.com>
2023-04-24 18:33:33 -0400

    all fixed

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const mysql = require('mysql2')
 const inquirer = require('inquirer')
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
-require ("console.table")
+require("console.table")
 const dbquery = ""
-const roless= []
-const departments=[ "math", "science"]
-
-
+const roless = []
+const departments = []
+const employees = []
+let update_emp
+let update_new_role
 
 // Connect to database
 const db = mysql.createConnection(
@@ -25,21 +26,30 @@ const db = mysql.createConnection(
 );
 
 function findroles() {
-  db.query(`SELECT title FROM roles`, (err, result) =>{
-    
+  db.query(`SELECT title FROM roles`, (err, result) => {
+
     for (var i = 0; i < result.length; i++) {
-     roless.push(result[i].title)
+      roless.push(result[i].title)
     }
-    
+
   })
 }
 
 function finddepartments() {
-  db.query(`SELECT NAME FROM department`, (err, result) =>{
+  db.query(`SELECT NAME FROM department`, (err, result) => {
     for (var i = 0; i < result.length; i++) {
       departments.push(result[i].NAME)
-     }
-     
+    }
+
+  })
+}
+
+function findEmployees() {
+  db.query(`SELECT last_name FROM employee`, (err, result) => {
+    for (var i = 0; i < result.length; i++) {
+      employees.push(result[i].last_name)
+    }
+    console.log(employees)
   })
 }
 
@@ -52,149 +62,189 @@ async function init() {
       choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
     }])
     .then((response) => {
-        if (response.init_prompt === 'view all departments') {
-          db.query("SELECT * FROM department", (err, result) => {
-            if (err) {
-              console.log(err);
-            }
-            console.table(result);
-            again(),
-          })
-
-
-        } else if (response.init_prompt === 'view all roles') {
-          db.query("SELECT * FROM roles", (err, result) => {
-            if (err) {
-              console.log(err);
-            }
-            console.table(result);
-            again(),
-
-          })
-        } else if (response.init_prompt === 'view all employees') {
-          db.query("SELECT * FROM employee", (err, result) => {
-            if (err) {
-              console.log(err);
-            }
-            console.table(result);
-            again(),
-
-          })
-        } else if (response.init_prompt === 'add a department') {
-          inquirer.prompt([{
-            type: 'maxlength-input',
-            message: 'What is the name of the department you would like to add??',
-            name: 'dep_name',
-            maxLength: 30
-          }]).then((response) => {
-              db.query(`INSERT INTO department(name) VALUES("${response.dep_name}")`, (err, result) => {
-                if (err) {
-                  console.log(err);
-                }
-                console.log("Department has been added");
-                again()
-          })
-            
-          })
-
-
-
-
-        } else if (response.init_prompt === 'add a role'){
-          inquirer.prompt([{
-            type: 'maxlength-input',
-            message: 'What is the name of the role you would like to add??',
-            name: 'role_name',
-            maxLength: 30
-          },
-          {
-            type: 'number',
-            message: 'What salary is associated with this role?',
-            name: 'role_salary'
-          },
-          {
-            
-            type: 'list',
-            message: 'What department is this new role associated with?',
-            name: 'role_department',
-            choices: departments
+      if (response.init_prompt === 'view all departments') {
+        db.query("SELECT * FROM department", (err, result) => {
+          if (err) {
+            console.log(err);
           }
-        ]).then((response)=>{
-             db.query(`SELECT id FROM department WHERE NAME = "${response.role_department}"`, (err, result)=>{
-              db.query(`INSERT INTO roles(title,salary, department_id) VALUES("${response.role_name}", ${response.role_salary}, ${result[0].id})`, (err, result) => {
-              if(err){
+          console.table(result);
+          again()
+        })
+
+
+
+      } else if (response.init_prompt === 'view all roles') {
+        db.query("SELECT * FROM roles", (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(result);
+          again()
+
+        })
+      } else if (response.init_prompt === 'view all employees') {
+        db.query("SELECT * FROM employee", (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+          console.table(result);
+          again()
+
+        })
+      } else if (response.init_prompt === 'add a department') {
+        inquirer.prompt([{
+          type: 'maxlength-input',
+          message: 'What is the name of the department you would like to add??',
+          name: 'dep_name',
+          maxLength: 30
+        }]).then((response) => {
+          db.query(`INSERT INTO department(name) VALUES("${response.dep_name}")`, (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log("Department has been added");
+            again()
+          })
+
+        })
+
+
+
+
+      } else if (response.init_prompt === 'add a role') {
+        inquirer.prompt([{
+          type: 'maxlength-input',
+          message: 'What is the name of the role you would like to add??',
+          name: 'role_name',
+          maxLength: 30
+        },
+        {
+          type: 'number',
+          message: 'What salary is associated with this role?',
+          name: 'role_salary'
+        },
+        {
+
+          type: 'list',
+          message: 'What department is this new role associated with?',
+          name: 'role_department',
+          choices: departments
+        }
+        ]).then((response) => {
+          db.query(`SELECT id FROM department WHERE NAME = "${response.role_department}"`, (err, result) => {
+            db.query(`INSERT INTO roles(title,salary, department_id) VALUES("${response.role_name}", ${response.role_salary}, ${result[0].id})`, (err, result) => {
+              if (err) {
                 console.log(err);
               }
               console.log("New role title has been added")
-              again(),
-            })})
-           
-            
-          })
-        } else if (response.init_prompt === 'add an employee'){
-          //next prompt is an idea for how to call the roles in an array and give it choice in the inquirer question. I need to then relate this to the 
-          let roles = db.query("SELECT title from roles")
-          inquirer.prompt([{
-            type: 'maxlength-input',
-            message: 'What is the first name of the employee you would like to add??',
-            name: 'emp_fname',
-            maxLength: 30
-          },
-          {
-            type: 'maxlength-input',
-            message: 'What is the last name of the employee you would like to add?',
-            name: 'emp_lname',
-            maxLength: 30
-          },
-          {
-            type: 'list' ,
-            message: 'What role does this person have?',
-            name: 'emp_role',
-            choices: roless
-          }
-        ]).then((response)=>{
-           //substituted values for role_id and manager_id. Fix when I can.
-           db.query(`SELECT id FROM roles WHERE title = "${response.emp_role}"`, (err, result)=>{
-            db.query(`INSERT INTO employee(first_name,last_name, role_id) VALUES("${response.emp_fname}", "${response.emp_lname}", ${result[0].id})`, (err, result) => {
-              if(err){
-                console.log(err);
-              }
-              console.log("New employee has been added")
-              again(),
+              again()
             })
           })
 
-          })
-        } else if (response.init_prompt === 'update an employee role'){
-          
+
+        })
+      } else if (response.init_prompt === 'add an employee') {
+        //next prompt is an idea for how to call the roles in an array and give it choice in the inquirer question. I need to then relate this to the 
+
+        inquirer.prompt([{
+          type: 'maxlength-input',
+          message: 'What is the first name of the employee you would like to add??',
+          name: 'emp_fname',
+          maxLength: 30
+        },
+        {
+          type: 'maxlength-input',
+          message: 'What is the last name of the employee you would like to add?',
+          name: 'emp_lname',
+          maxLength: 30
+        },
+        {
+          type: 'list',
+          message: 'What role does this person have?',
+          name: 'emp_role',
+          choices: roless
         }
-      
-    })}
-      //})*/
-    
+        ]).then((response) => {
+          //substituted values for role_id and manager_id. Fix when I can.
+          db.query(`SELECT id FROM roles WHERE title = "${response.emp_role}"`, (err, result) => {
+            db.query(`INSERT INTO employee(first_name,last_name, role_id) VALUES("${response.emp_fname}", "${response.emp_lname}", ${result[0].id})`, (err, result) => {
+              if (err) {
+                console.log(err);
+              }
+              console.log("New employee has been added")
+              again()
+            })
+          })
+
+        })
+      } else if (response.init_prompt === 'update an employee role') {
+        inquirer.prompt([{
+          type: 'list',
+          message: 'Which employee would you like to update?',
+          name: 'update_emp',
+          choices: employees
+        },
+        {
+          type: 'list',
+          message: 'What role has this employee moved into?',
+          name: 'update_new_role',
+          choices: roless
+        }
+        ]).then((response) => {
+          db.query(`SELECT id FROM employee WHERE last_name ="${response.update_emp}"`, (err, result) => {
+            console.log(result[0].id);
+            update_emp = result[0].id;
+            console.log(update_emp)
+          })
+          db.query(`SELECT id FROM roles WHERE title ="${response.update_new_role}"`, (err, result) => {
+            console.log(result[0].id);
+            update_new_role = result[0].id;
+            console.log(update_new_role)
+          /*})
+          .then(() => {*/
+            console.log("line 204", update_emp)
+            console.log("line 205", update_new_role)
+            db.query(`UPDATE employee SET role_id = ${update_new_role} WHERE id = ${update_emp}`, (err, result) => {
+              if (err) {
+                console.log(err)
+              }
+              console.log(`Employee has been updated`)
+              again()
+            })
+          })
+        })
+          
+      }
+    })
+}
 
 
 
-function again(){
+
+
+
+
+function again() {
   inquirer.prompt([{
-    type: 'list' ,
+    type: 'list',
     message: 'Would you like to complete another task?',
     name: 'taskagain',
     choices: ['yes', 'no']
   }])
-  .then((response)=>{
-    if(response.taskagain === 'yes'){
-      init();
-    } if (response.taskagain ==='no'){
-      console.log('Have a nice day!');
-      return
-    }
-  })
+    .then((response) => {
+      if (response.taskagain === 'yes') {
+        init();
+      } if (response.taskagain === 'no') {
+        console.log('Have a nice day!');
+        process.exit();
+      }
+    })
 }
 
 
 findroles();
-finddepartments();      
+finddepartments();
+findEmployees();
 init();
 
 
